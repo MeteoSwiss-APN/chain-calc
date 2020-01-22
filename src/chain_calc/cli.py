0202c721e2135611d6089625fa5ce295c63186ba
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-01-22 11:39:45 +0100

    restructure basic command line interface and its tests
    add single number as mandatory argument

--- src/chain_calc/cli.py
+++ src/chain_calc/cli.py
@@ -9,42 +9,71 @@ from . import __version__
 from .utils import count_to_log_level
 
 
+def print_help(ctx, param, value):
+    if value:
+        click.echo(ctx.get_help())
+        if param is None:
+            ctx.exit(1)
+        ctx.exit(0)
+
+
+def print_version(ctx, param, value):
+    if value:
+        click.echo(__version__)
+        ctx.exit(0)
+
+
 @click.command()
+@click.argument("number", metavar="NUMBER", nargs=-1)
+@click.option(
+    "--help",
+    "-h",
+    help="Show this message and exit.",
+    is_flag=True,
+    expose_value=False,
+    callback=print_help,
+)
+@click.option(
+    "--version",
+    "-V",
+    help="Print version and exit.",
+    is_flag=True,
+    expose_value=False,
+    callback=print_version,
+)
 @click.option(
     "--dry-run",
     "-n",
     flag_value="dry_run",
     default=False,
-    help="Perform a trial run with no changes made",
+    help="Perform a trial run with no changes made.",
 )
 @click.option(
     "--verbose",
     "-v",
     count=True,
-    help="Increase verbosity (specify multiple times for more)",
+    help="Increase verbosity (specify multiple times for more).",
 )
-@click.option("--version", "-V", is_flag=True, help="Print version")
-def main(*args, **kwargs):
+@click.pass_context
+def main(ctx, number, dry_run, verbose):
     """Console script for test_cli_project."""
 
-    logging.basicConfig(level=count_to_log_level(kwargs["verbose"]))
+    if dry_run:
+        click.echo("This is merely a dry run")
+        return 0
 
+    if len(number) == 1:
+        number = next(iter(number))
+        click.echo(number)
+    else:
+        if len(number) > 1:
+            click.echo(f"Error: expecting 1 number, got {len(number)}")
+        print_help(ctx, None, True)
+
+    logging.basicConfig(level=count_to_log_level(verbose))
     logging.warning("This is a warning.")
     logging.info("This is an info message.")
     logging.debug("This is a debug message.")
-
-    if kwargs["version"]:
-        click.echo(__version__)
-        return 0
-
-    if kwargs["dry_run"]:
-        click.echo("Is dry run")
-        return 0
-
-    click.echo(
-        "Replace this message by putting your code into test_cli_project.cli.main"
-    )
-    click.echo("See click documentation at http://click.pocoo.org/")
 
     return 0
 
--- tests/chain_calc/test_cli.py
+++ tests/chain_calc/test_cli.py
@@ -1,28 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Tests for package `chain_calc`."""
-import pytest
+#import pytest
 from click.testing import CliRunner
 
 from chain_calc import cli
 
 
-# dummy lines to avoid flake8 error F401 (imported but unused); remove!
-pytest
+class _TestCLI:
+    """Base class to test the command line interface."""
 
+    def call(self, args=None):
+        runner = CliRunner()
+        return runner.invoke(cli.main, args)
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert "test_cli_project.cli.main" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
-    assert help_result.exit_code == 0
-    assert "Show this message and exit." in help_result.output
-    dry_run_result = runner.invoke(cli.main, ["-n"])
-    assert dry_run_result.exit_code == 0
-    assert "Is dry run" in dry_run_result.output
-    version_result = runner.invoke(cli.main, ["-V"])
-    assert version_result.exit_code == 0
-    assert cli.__version__ in version_result.output
+class TestBasic(_TestCLI):
+    """Test basic options."""
+
+    def test_default(self):
+        result = self.call()
+        assert result.exit_code == 1
+        assert result.output.startswith("Usage: ")
+        assert "Show this message and exit." in result.output
+
+    def test_help(self):
+        result = self.call(["--help"])
+        assert result.exit_code == 0
+        assert result.output.startswith("Usage: ")
+        assert "Show this message and exit." in result.output
+
+    def test_version(self):
+        result = self.call(["-V"])
+        assert result.exit_code == 0
+        assert cli.__version__ in result.output
+
+    def test_dry_run(self):
+        result = self.call(["-n"])
+        assert result.exit_code == 0
+        assert "This is merely a dry run" in result.output
+
+class TestCalcInt(_TestCLI):
+    """Test calculations with integers."""
+
+    def test_only_arg(self):
+        result = self.call(["4"])
+        assert result.exit_code == 0
+        assert result.output.strip() == "4"
+
+    def test_two_args(self):
+        result = self.call(["4", "5"])
+        assert result.exit_code == 1
+        assert result.output.startswith("Error: expecting 1 number, got 2")
